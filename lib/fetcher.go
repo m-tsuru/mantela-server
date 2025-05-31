a1b09ad9c834e49d247e3331ab03961f1a409026
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2025-06-01 03:01:41 +0900

    Add go.mod and go.sum files for dependency management

--- lib/fetcher.go
+++ lib/fetcher.go
@@ -1,1 +1,14 @@
 package mantela_fetcher
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func ParseMantela(resp *http.Response) (*Mantela, error) {
+	var mantela Mantela
+	if err := json.NewDecoder(resp.Body).Decode(&mantela); err != nil {
+		return nil, err
+	}
+	return &mantela, nil
+}
