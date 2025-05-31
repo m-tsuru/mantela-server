8ae735ae567778e0dd7c949abad397a29daae13f
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2025-06-01 02:58:26 +0900

    Add omitempty tags to Mantela struct fields and allow null values

--- lib/mantela.go
+++ lib/mantela.go
@@ -4,33 +4,33 @@ package mantela_fetcher
 // 交換局の相互接続情報を提供します。
 type Mantela struct {
 	// Mantela のバージョンを指定します（通常は '0.0.0' を指定してください）
-	Version *string `json:"version"`
+	Version *string `json:"version,omitempty"`
 	// あなた自身の交換局の情報を提供してください
-	AboutMe *AboutMe `json:"aboutMe"`
+	AboutMe *AboutMe `json:"aboutMe,omitempty"`
 	// あなた自身の交換局に接続されている端末の情報を提供してください
-	Extensions []Extension `json:"extensions"` // 配列自体はnullになり得ますが、要素はnilポインタ
+	Extensions []Extension `json:"extensions,omitempty"` // 配列自体はnullになり得ますが、要素はnilポインタ
 	// 接続されている他の交換局の情報を提供してください
-	Providers []Provider `json:"providers"` // 配列自体はnullになり得ますが、要素はnilポインタ
+	Providers []Provider `json:"providers,omitempty"` // 配列自体はnullになり得ますが、要素はnilポインタ
 }
 
 // AboutMe はあなた自身の交換局の情報を提供します。
 type AboutMe struct {
 	// 交換局の名称を人間の読める形式で指定してください
-	Name *string `json:"name"`
+	Name *string `json:"name,omitempty"`
 	// 交換局の好ましいプレフィクス番号を指定してください
-	PreferredPrefix interface{} `json:"preferredPrefix"` // string または []string, ポインタ型にすると複雑になるためinterface{}のまま
+	PreferredPrefix interface{} `json:"preferredPrefix,omitempty"` // string または []string, ポインタ型にすると複雑になるためinterface{}のまま
 	// 交換局の識別子を機械の読める形式で指定してください
-	Identifier *string `json:"identifier"`
+	Identifier *string `json:"identifier,omitempty"`
 	// 交換局の SIP ユーザ名を指定してください
-	SIPUsername *string `json:"sipUsername"`
+	SIPUsername *string `json:"sipUsername,omitempty"`
 	// 交換局の SIP パスワードを指定してください
-	SIPPassword *string `json:"sipPassword"`
+	SIPPassword *string `json:"sipPassword,omitempty"`
 	// 交換局の SIP ホスト名や IP アドレスを指定してください
-	SIPServer *string `json:"sipServer"`
+	SIPServer *string `json:"sipServer,omitempty"`
 	// 交換局の SIP ポート名やポート番号を指定してください
-	SIPPort *string `json:"sipPort"`
+	SIPPort *string `json:"sipPort,omitempty"`
 	// 交換局が（一時的に）利用できないとき、true を指定してください
-	Unavailable *bool `json:"unavailable"`
+	Unavailable *bool `json:"unavailable,omitempty"`
 }
 
 // Extension はあなた自身の交換局に接続されている端末の情報を提供します。
@@ -38,15 +38,15 @@ type AboutMe struct {
 // フィールド内の個々の値はnilになり得ます。
 type Extension struct {
 	// 端末の名称を人間の読める形式で指定してください
-	Name *string `json:"name"`
+	Name *string `json:"name,omitempty"`
 	// 端末に紐付けられる内線番号を指定してください
-	Extension *string `json:"extension"`
+	Extension *string `json:"extension,omitempty"`
 	// 端末の識別子を機械の読める形式で指定してください
-	Identifier *string `json:"identifier"`
+	Identifier *string `json:"identifier,omitempty"`
 	// 端末の種別を指定してください
-	Type *string `json:"type"`
+	Type *string `json:"type,omitempty"`
 	// 転送を伴うような内線番号において、転送先の端末の識別子を指定してください
-	TransferTo []string `json:"transferTo"` // 配列自体はnullになり得ますが、要素はnilポインタ
+	TransferTo []string `json:"transferTo,omitempty"` // 配列自体はnullになり得ますが、要素はnilポインタ
 }
 
 // Provider は接続されている他の交換局の情報を提供します。
@@ -54,11 +54,11 @@ type Extension struct {
 // フィールド内の個々の値はnilになり得ます。
 type Provider struct {
 	// 交換局の名称を人間の読める形式で指定してください
-	Name *string `json:"name"`
+	Name *string `json:"name,omitempty"`
 	// 交換局に紐付けられるプレフィクス番号を指定してください
-	Prefix *string `json:"prefix"`
+	Prefix *string `json:"prefix,omitempty"`
 	// 交換局の識別子を機械の読める形式で指定してください
-	Identifier *string `json:"identifier"`
+	Identifier *string `json:"identifier,omitempty"`
 	// その交換局の mantela.json の URL を指定してください
-	Mantela *string `json:"mantela"`
+	Mantela *string `json:"mantela,omitempty"`
 }
